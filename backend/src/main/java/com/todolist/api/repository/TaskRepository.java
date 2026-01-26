927498dc40734e27c6baad6896aad57f2972eb8d
Mateus Redivo <80864178+Mateus-Redivo@users.noreply.github.com>
2026-01-26 09:02:48 -0300

    feat: implement logic to find the lowest available task ID during task creation

--- backend/src/main/java/com/todolist/api/repository/TaskRepository.java
+++ backend/src/main/java/com/todolist/api/repository/TaskRepository.java
@@ -1,11 +1,16 @@
 package com.todolist.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.todolist.api.model.Task;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>{
     
+    @Query("SELECT t.id FROM Task t ORDER BY t.id ASC")
+    List<Long> findAllIds();
 }
